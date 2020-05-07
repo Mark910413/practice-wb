45e46f3bf6c3b856ceaad3770474ffdc27807513
Micheal <694423819@qq.com>
2020-05-08 00:38:48 +0800

    output management

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import _ from 'lodash';
 import './style.css';
-import Pic from './pic.png';
+import printMe from './print';
 
 function component() {
   var element = document.createElement('div');
-  var img = new Image();
-  img.src = Pic;
+  var btn = document.createElement('button');
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
-  element.appendChild(img); 
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
   return element;
 }
 
