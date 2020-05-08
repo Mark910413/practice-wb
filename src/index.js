98ce54d98a3c6c6e860e10615d361f20f3d6756b
柳哲 <liu_zhe@gzhc365.com>
2020-05-08 17:54:12 +0800

    1 webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ import printMe from './print';
 function component() {
   var element = document.createElement('div');
   var btn = document.createElement('button');
+  var dom = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
 
   btn.innerHTML = 'Click me and check the console change!';
-  btn.onclick = printMe;
+  btn.onclick = printMe; 
   element.appendChild(btn);
+
+  dom.innerHTML = '<img  src="./img/pic.png" width="100px" />';
+
+  element.appendChild(dom); 
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const  CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
     app: './src/index.js',
-    print: './src/print.js'
+    print: './src/print.js',
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   mode: 'development',
   module: {
@@ -27,11 +28,12 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin({dry: true}),
     new HtmlWebpackPlugin({title: 'Output Management', template: './public/index.html', filename: 'index.html'}),
+    new CopyWebpackPlugin([{from: path.resolve(__dirname, 'static'), to: ''}])
   ],
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
     compress: true,
-    port: 3000
+    port: 3000,
   }
 }
