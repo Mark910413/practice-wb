f7b78afcc76594877472b452d1f34bae27e9c3ec
柳哲 <liu_zhe@gzhc365.com>
2020-05-09 15:17:22 +0800

    1 express practice

---
+++ express.md
@@ -0,0 +1,3 @@
+## app.use
+
+### 与app.get,app.post,app.all作几乎相同， 不过callback可以接受中间函数用法更加强大全面；
--- server.js
+++ server.js
@@ -8,8 +8,18 @@ app.use(express.static(path.resolve(__dirname, 'static')));
 app.use('static', express.static(path.resolve(__dirname, 'static')));
 
 app.get('/api/getmsg', (req, res, next) => {
-  res.send('get a messgae from server!');
+  res.send('Get a messgae from server!');
 })
+
+app.use((req, res, next) => {
+  res.render('public/index.html', (err, html) => {
+    if (!err) {
+      console.log(err);
+    } else {
+      res.send(html);
+    }
+  });
+});
 
 app.listen(3000, (err) => {
   if (!err) {
