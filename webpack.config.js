3ee8a2a74a25642f280bc7ebc3d959a80eb384af
柳哲 <liu_zhe@gzhc365.com>
2020-06-28 16:09:04 +0800

    1  first-commit

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,19 @@ module.exports = {
         use: [
           'file-loader'
         ]
+      },
+      {
+        test: /\.js$/, use: [{
+          loader: '@babel/loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              {
+                'useBulitlns': 'usage',
+              }
+            ]
+          }
+        }]
       }
     ]
   }
