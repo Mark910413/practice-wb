44e145b0444e0a6587161a98fca4e3adc3899abf
柳哲 <liu_zhe@gzhc365.com>
2020-06-30 17:24:31 +0800

    1 A-finish

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
-import './style.css';
+import style from './style.css';
 import printMe from './print';
-
+import Pic from '../static/img/pic.png';
 function component() {
   var element = document.createElement('div');
   var btn = document.createElement('button');
@@ -9,16 +9,16 @@ function component() {
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  element.classList.add(style.bg);
 
-  btn.innerHTML = 'Click me and check the console change!';
+  btn.innerHTML = `<div class="${style.hello}">  Click me and check the console change!</div>`;
   btn.onclick = printMe; 
   element.appendChild(btn);
 
-  dom.innerHTML = '<img  src="./img/pic.png" width="100px" />';
-
+  dom.innerHTML = `<img  src='${Pic}' width="100px" />`;
+  dom.classList.add(style.hello);
   element.appendChild(dom); 
-
+  
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,56 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const  CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const postcssSprite = require('postcss-sprites');
+const autoprefixer = require('autoprefixer');
 const webpack = require('webpack');
+console.log('---------------path--------------------', path.resolve(__dirname, './static/img'));
 module.exports = {
   entry: {
     app: './src/index.js',
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    alias: {
+      '@img': path.resolve(__dirname, './static/img')
+    }
   },
   mode: 'development',
   module: {
     rules: [
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      { test: /\.(png|svg|jpg|gif)$/, use: ['file-loader'] },
+      { 
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 2,
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                autoprefixer(['IE 10']),
+                postcssSprite(path.resolve(__dirname, './static/img')),
+              ]
+            }
+          }
+        ]
+      },
+      { test: /\.(png|svg|jpg|gif)$/, use: [{loader: 'url-loader', options: {limit: 3 * 1024 }}]},
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
-          'file-loader'
+          'url-loader'
         ]
       },
-      {
-        test: /\.js$/,
-        use: [
-              {
-                  loader: 'babel-loader',
-                  options: {
-                      presets: [
-                          [
-                              '@babel/preset-env',
-                              {
-                                  useBuiltIns: 'usage',
-                                  'corejs': 3
-                              }
-                          ]
-                      ]
-                  }
-              }
-        ]
-      }
     ]
   },
   plugins: [
@@ -48,7 +59,11 @@ module.exports = {
     new HtmlWebpackPlugin({title: 'Output Management', template: './public/index.html', filename: 'index.html'}),
     new CopyWebpackPlugin([{from: path.resolve(__dirname, 'static'), to: ''}]),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin()
+    new webpack.NamedModulesPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
   ],
   devtool: 'inline-source-map',
   devServer: {
